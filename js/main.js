31bff597bb3c6e489ebcf32288fa3c0353aa2e06
EldorKenjabayev <eldorkenjabayev14@gmail.com>
2023-04-14 15:44:44 +0300

    find the sum of digits of a number

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let c = +prompt('to`rt xonali sonni kiriting :::')
+c1 = c % 10
+c2 = c % 100
+c3 =  Math.floor(c2 /10) 
+c4 = c % 1000
+c5 = Math.floor(c4 / 100)
+c6 = c % 10000
+c7 = Math.floor(c6 / 1000)
+alert(c1+c3+c5+c7)
+
+let a = +prompt('uch xonali sonni kiriting :::' )
+a1 = a % 10
+a2 = a % 100
+a3 =  Math.floor(a2 /10) 
+a4 = a % 1000
+a5 = Math.floor(a4 / 100)
+alert(a5 + a3 + a1) 
+
+
+
+let b = +prompt('ikki xonali sonini kiriting :::')
+b1 = b % 10
+b2 = b % 100
+b3 = Math.floor(b2 / 10)
+alert(b1 + b3)
